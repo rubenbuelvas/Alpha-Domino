87b37dee8299e4d03c4023369eb78f41bbe189fa
Ruben Buelvas <rcbuelvasv@gmail.com>
2019-07-14 18:12:00 -0500

    Stuff

--- game.py
+++ game.py
@@ -54,15 +54,28 @@ def turn_tile(tile):
 
 	
 def play(agents, env, verbose=False):
-	
 	winner = env.get_winner()
+	turn = 0
+
+	first_tile = [-1, -1]
+	first_agent = 0
+	first_tile_pos = 0
+	for i in range(len(agents)):
+		for j in range(len(agents[i].hand)):
+			if agents[i].hand[j, 0] > first_tile[0] and agents[i].hand[j, 0] == agents[i].hand[j, 1]:
+				first_tile = agents[i].hand[j]
+				first_agent = i
+				first_tile_pos = j
+	if first_tile != [-1, -1]:
+		
+
 	while(winner == -1):
+		turn += 1
 		for agent in agents:
 			played_tile = agent.act(env.get_observation())
 			winner = env.get_winner()
 			if winner != -1:
 				break
-			print(len(agent.hand))
 
 	print(f"Player {winner} won!")
 		
@@ -72,7 +85,5 @@ for i in range(4):
 	agents.append(RandomAgent())
 
 env = Environment(agents)
-print(agents[0].hand)
-
 
 play(agents, env, verbose=True)
