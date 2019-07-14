9b40fc1f69da83de50220f1855b593c09dda47d1
Ruben Buelvas <rcbuelvasv@gmail.com>
2019-07-14 13:35:34 -0500

    Structure

--- game.py
+++ game.py
@@ -1,27 +1,78 @@
 import numpy as np
+import random
 
-class Environment(object):
-	"""docstring for Environment"""
-	def __init__(self, arg):
-		super(Environment, self).__init__()
-		self.arg = arg
+class Environment():
 	
-class RandomAgent(object):
-		"""docstring for RandomAgent"""
-		def __init__(self, arg):
-			super(RandomAgent, self).__init__()
-			self.arg = arg
+	def __init__(self, agents, n_players=4, tiles_per_player=5):
+		self.n_players = n_players
+		self.agents = agents
+		self.pile = generate_tiles()
+		self.hand_sizes = []
+		for agent in agents:
+			for i in range(tiles_per_player):
+				agent.hand.append(self.pile.pop())
+				self.hand_sizes.append(len(agent.hand))
+		self.table = []
 
-def generateTiles():
+	def recalculate_hand_sizes(self):
+		for i in range(self.n_players):
+			self.hand_sizes[i] = len(self.agents[i].hand)
+
+	def get_observation(self):
+		return -1
+
+	def get_winner(self):
+		self.recalculate_hand_sizes()
+		winner = -1
+		for i in range(len(self.hand_sizes)):
+			if self.hand_sizes[i] == 0:
+				winner = i
+		return winner
+
+
+class RandomAgent():
+	def __init__(self):
+		self.hand = []
+
+	def act(self, observation):
+		tile = self.hand.pop(random.randrange(len(self.hand)))
+		return tile
+
+
+def generate_tiles(max_value=6):
 	tiles = []
-	for i in range(7):
+	for i in range(max_value+1):
 		for j in range(i+1):
 			tiles.append([i, j])
-	print(tiles)
-	print(len(tiles))
+	random.shuffle(tiles)
+	return tiles
+
+
+def turn_tile(tile):
+	new_tile = [tile[1], tile[0]]
+	return new_tile
+
+	
+def play(agents, env, verbose=False):
+	
+	winner = env.get_winner()
+	while(winner == -1):
+		for agent in agents:
+			played_tile = agent.act(env.get_observation())
+			winner = env.get_winner()
+			if winner != -1:
+				break
+			print(len(agent.hand))
+
+	print(f"Player {winner} won!")
 		
-def play(env, agents, verbose=False):
-	pass
+
+agents = []
+for i in range(4):
+	agents.append(RandomAgent())
+
+env = Environment(agents)
+print(agents[0].hand)
 
 
-generateTiles()
+play(agents, env, verbose=True)
